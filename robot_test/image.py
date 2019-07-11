f189699a1ba4080c21a5abc68501847ac422bec3
fanfate <fan1090682278@outlook.com>
2019-07-11 17:34:32 +0800

    更新新的二维码定位方法

--- robot_test/image.py
+++ robot_test/image.py
@@ -80,34 +80,34 @@ for v in vision:
         ts.append(t)
     trans.append(ts)
 
-vv = trans[0]
+# vv = trans[0]
 
 
-row = 720
-col = 1280
-re = []
-for i in range(row):
-    t = []
-    for j in range(col):
-        s = []
-        for k in range(3):
-            m = vv[i * col * 3 + j * 3 + k]
-            s.append(m)
-        s[0], s[2] = s[2], s[0]  # opencv使用的是BGR，因此需要交换G和B
-        t.append(s)
-    re.append(t)
+# row = 720
+# col = 1280
+# re = []
+# for i in range(row):
+#     t = []
+#     for j in range(col):
+#         s = []
+#         for k in range(3):
+#             m = vv[i * col * 3 + j * 3 + k]
+#             s.append(m)
+#         s[0], s[2] = s[2], s[0]  # opencv使用的是BGR，因此需要交换G和B
+#         t.append(s)
+#     re.append(t)
 
 
-# f = open('00.txt', 'w')
-# f.write(str(re))
-# f.close()
+# # f = open('00.txt', 'w')
+# # f.write(str(re))
+# # f.close()
 
-mat = np.array(re,dtype=np.uint8)
-# mat1 = mat
-mat1 = cv2.flip(mat,0,dst=None) #垂直镜像
+# mat = np.array(re,dtype=np.uint8)
+# # mat1 = mat
+# mat1 = cv2.flip(mat,0,dst=None) #垂直镜像
 
-cv2.imshow('hhh',mat1)
-cv2.waitKey()
+# cv2.imshow('hhh',mat1)
+# cv2.waitKey()
 
 
 # -- 二维码识别 --
@@ -173,26 +173,26 @@ cv2.waitKey()
 
 # -- 圆柱体物体识别 --
 
-im_hsv = cv2.cvtColor(mat1,cv2.COLOR_BGR2HSV) # 转换为HSV
+# im_hsv = cv2.cvtColor(mat1,cv2.COLOR_BGR2HSV) # 转换为HSV
 
 
-lower_red = np.array([0,100,100])
-upper_red = np.array([10,255,255])
-red_mask = cv2.inRange(im_hsv,lower_red,upper_red) # 红色区域取255（白色），其余取0（黑色）
+# lower_red = np.array([0,100,100])
+# upper_red = np.array([10,255,255])
+# red_mask = cv2.inRange(im_hsv,lower_red,upper_red) # 红色区域取255（白色），其余取0（黑色）
 
-binary = cv2.Canny(red_mask, 0, 60, apertureSize = 3)
-contours, cnt = cv2.findContours(binary,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE) # 提取矩形轮廓
+# binary = cv2.Canny(red_mask, 0, 60, apertureSize = 3)
+# contours, cnt = cv2.findContours(binary,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE) # 提取矩形轮廓
 
-if len(contours) > 0:
-    ll = contours[0].mean(0)[0]
+# if len(contours) > 0:
+#     ll = contours[0].mean(0)[0]
 
-    center_x = ll[0]
-    center_y = ll[1]
-    print(center_x)
-    print(center_y) # 找到的像素点坐标（也可选择不取整）
-    # red_mask[center_y][center_x] = 0
-    cv2.imshow('mask',red_mask)
-    cv2.waitKey()
+#     center_x = ll[0]
+#     center_y = ll[1]
+#     print(center_x)
+#     print(center_y) # 找到的像素点坐标（也可选择不取整）
+#     # red_mask[center_y][center_x] = 0
+#     cv2.imshow('mask',red_mask)
+#     cv2.waitKey()
 
 
 # -- 圆柱体物体识别 --
@@ -259,6 +259,76 @@ if len(contours) > 0:
 # -- T、E台子位置识别 --
 
 
+# -- 寻找二维码新方法  -- 
+
+limit = 50
+tx = []
+ty = []
+masks = []
+
+row = 720
+col = 1280
+
+
+tx = []
+ty = []
+
+for q in range(zedNum):
+    v = trans[q]
+    re = []
+    for i in range(row):
+        t = []
+        for j in range(col):
+            s = []
+            for k in range(3):
+                m = v[i * col * 3 + j * 3 + k]
+                s.append(m)
+            s[0], s[2] = s[2], s[0]  # opencv使用的是BGR，因此需要交换G和B
+            t.append(s)
+        re.append(t)
+    
+    mat = np.array(re,dtype=np.uint8)
+    mat1 = cv2.flip(mat,0,dst=None) #垂直镜像
+    gray = cv2.cvtColor(mat1,cv2.COLOR_BGR2GRAY)
+    mask = cv2.inRange(gray,limit,255) #将limit-255范围内的像素点全部转化为白色（255），0-limit为黑色（0）,达到凸显二维码的目的
+    masks.append(mask)
+
+    kernel = np.ones((5,5),np.uint8)
+    erosion = cv2.erode(mask,kernel)
+    erosion = cv2.erode(erosion,kernel)
+
+    contours,hier=cv2.findContours(erosion, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    rec = []
+    for c in contours:
+        x,y,w,h=cv2.boundingRect(c)#计算出一个简单地边界框
+        if (abs(w-h)<10) & (w>10): # 判断得到的矩形的大小是否符合（二维码的某个块即可）
+            rec.append([x,y,w,h])
+
+    mi = -1
+    mw = 0
+    l = len(rec)
+    print(l)
+    for i in range(l):
+        x,y,w,h = rec[i]
+        if (mw < w):
+            mw = w
+            mi = i
+    if mi > -1:
+        x,y,w,h = rec[mi]
+        # mmm = mat1[y:y+h, x:x+w]
+        # cv2.imshow('mmm',mmm)
+        # cv2.waitKey()
+        targetX = x + w / 2
+        targetY = y + h / 2
+        tx.append(targetX)
+        ty.append(targetY)
+        print(targetX)
+        print(targetY)
+    else:
+        print(str(q) + ' is error!')
+
+
+# -- 寻找二维码新方法  -- 
 
 
 
